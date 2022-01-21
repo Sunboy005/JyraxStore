5e3d9e5deb407878862192b0a88482b659aab4be
Amos Alagbe <sunboy005@gmail.com>
2022-01-21 09:36:32 +0100

    blank Commit

---
+++ JyraxStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JyraxStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
