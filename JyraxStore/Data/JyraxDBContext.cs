65dbb2daad9f9eaf4cf00f7a355114370187641d
Amos Alagbe <sunboy005@gmail.com>
2022-01-21 11:50:04 +0100

    InitialCommit

---
+++ JyraxStore/Data/JyraxDBContext.cs
@@ -0,0 +1,18 @@
+﻿using JyraxStore.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace JyraxStore.Data
+{
+    public class JyraxDBContext:IdentityDbContext
+    {
+        public JyraxDBContext(DbContextOptions<JyraxDBContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
---
+++ JyraxStore/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JyraxStore.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public string CategoryName { get; set; }
+        public string CategoryDescription { get; set; }
+        public string DisplayOrder { get; set; }
+
+
+
+
+    }
+}
